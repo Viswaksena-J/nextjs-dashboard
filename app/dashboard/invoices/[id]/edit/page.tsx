b1191d74237cab99ba61399da10d08a4f5a0d606
Viswaksena Jayam <viswaksene111@gmail.com>
2024-03-28 20:58:12 +0530

    Handling Errors

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -1,6 +1,7 @@
 import Form from '@/app/ui/invoices/edit-form';
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
 import { fetchInvoiceById, fetchCustomers } from '@/app/lib/data';
+import { notFound } from 'next/navigation';
 
  
 export default async function Page({ params }: { params: { id: string } }) {
@@ -8,7 +9,12 @@ export default async function Page({ params }: { params: { id: string } }) {
     const [invoice, customers] = await Promise.all([
         fetchInvoiceById(id),
         fetchCustomers(),
-      ]);
+    ]);
+
+    if (!invoice) {
+        notFound();
+    }
+
     return (
     <main>
       <Breadcrumbs
